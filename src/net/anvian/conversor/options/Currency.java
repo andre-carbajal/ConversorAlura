397c8fc857a18eacd1b4e51dec591acd4fb4f8cd
anviaan <andre.carbajal.vargas@gmail.com>
2023-07-11 17:07:56 -0500

    refactor

--- src/net/anvian/conversor/options/Currency.java
+++ src/net/anvian/conversor/options/Currency.java
@@ -7,24 +7,23 @@ public class Currency {
         try {
             String[] currencies = {"De Soles a Dolar", "De Soles a Euro", "De Soles a Libras", "De Soles a Yen", "De Soles a Won sul-coreano" +
                     "De Dolar a Soles", "De Euro a Soles", "De Libras a Soles", "De Yen a Soles", "De Won sul-coreano a Soles"};
+            String option = (JOptionPane.showInputDialog(null, "Monedas", "Entrada",JOptionPane.INFORMATION_MESSAGE, null, currencies, currencies[0])).toString();
 
             String currency = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero que deseas convertir:");
             currency = currency.replace(",", ".");
             double money = Double.parseDouble(currency);
 
-            String option = (JOptionPane.showInputDialog(null, "Monedas", "Entrada",JOptionPane.INFORMATION_MESSAGE, null, currencies, currencies[0])).toString();
-
             switch (option) {
-                case "De Soles a Dolar" -> TransformAndShow(money, 0.28, "Dolares");
-                case "De Soles a Euro" -> TransformAndShow(money, 0.25, "Euros");
-                case "De Soles a Libras" -> TransformAndShow(money, 0.21, "Libras");
-                case "De Soles a Yen" -> TransformAndShow(money, 38.78, "Yenes");
-                case "De Soles a Won sul-coreano" -> TransformAndShow(money, 356.63,"Won sul-coreano");
-                case "De Dolar a Soles" -> TransformAndShow(money, 3.63, "Soles");
-                case "De Euro a Soles" -> TransformAndShow(money, 4.0, "Soles");
-                case "De Libras a Soles" -> TransformAndShow(money, 4.67, "Soles");
-                case "De Yen a Soles" -> TransformAndShow(money, 0.026, "Soles");
-                case "De Won sul-coreano a Soles" -> TransformAndShow(money, 0.0028, "Soles");
+                case "De Soles a Dolar" -> TransformAndShow(money, 0.28, "Soles", "Dolares");
+                case "De Soles a Euro" -> TransformAndShow(money, 0.25, "Soles", "Euros");
+                case "De Soles a Libras" -> TransformAndShow(money, 0.21, "Soles", "Libras");
+                case "De Soles a Yen" -> TransformAndShow(money, 38.78, "Soles", "Yenes");
+                case "De Soles a Won sul-coreano" -> TransformAndShow(money, 356.63, "Soles", "Won sul-coreano");
+                case "De Dolar a Soles" -> TransformAndShow(money, 3.63, "Dolares", "Soles");
+                case "De Euro a Soles" -> TransformAndShow(money, 4.0, "Euro", "Soles");
+                case "De Libras a Soles" -> TransformAndShow(money, 4.67, "Libras", "Soles");
+                case "De Yen a Soles" -> TransformAndShow(money, 0.026, "Yen", "Soles");
+                case "De Won sul-coreano a Soles" -> TransformAndShow(money, 0.0028, "Won sul-coreano", "Soles");
             }
 
             int resp = JOptionPane.showConfirmDialog(null, "¿Desea continuar?", "Select an Opion", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
@@ -39,9 +38,8 @@ public class Currency {
         }
     }
 
-    private static void TransformAndShow(double value, double exchange, String txt){
-        value *= exchange;
-        value = (double) Math.round(value * 100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $"+ value + " " + txt);
+    private static void TransformAndShow(double value, double exchange, String from, String to){
+        double transform = (double) Math.round(value * exchange * 100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes " + value + " " + from + " equivalente a " + transform + " " + to);
     }
 }
--- src/net/anvian/conversor/options/Temperature.java
+++ src/net/anvian/conversor/options/Temperature.java
@@ -7,20 +7,19 @@ import javax.swing.*;
 public class Temperature {
     public static void init() {
         try {
-            String[] temperatures = {"De Celsius a Fahrenheit", "De Celsius a Kelvin", "De Celsius a Rankine", "De Fahrenheit a Celsius", "De Fahrenheit a Kelvin", "De Fahrenheit a Rankine"};
+            String[] temperatures = {"De Celsius a Fahrenheit", "De Celsius a Kelvin", "De Fahrenheit a Celsius", "De Fahrenheit a Kelvin", "De Kelvin a Celsius", "De Kelvin a Fahrenheit"};
+            String option = (JOptionPane.showInputDialog(null, "Temperature", "Entrada",JOptionPane.INFORMATION_MESSAGE, null, temperatures, temperatures[0])).toString();
 
             String temperature = JOptionPane.showInputDialog(null, "Ingrese la cantidad que deseas convertir:");
             double grades = Double.parseDouble(temperature);
 
-            String option = (JOptionPane.showInputDialog(null, "Temperature", "Entrada",JOptionPane.INFORMATION_MESSAGE, null, temperatures, temperatures[0])).toString();
-
             switch (option) {
-                case "De Celsius a Fahrenheit" -> Temperatures.CelsiusToFahrenheit(grades);
-                case "De Celsius a Kelvin" -> Temperatures.CelsiusToKelvin(grades);
-                case "De Celsius a Rankine" -> Temperatures.CelsiusToRankine(grades);
-                case "De Fahrenheit a Celsius" -> Temperatures.FahrenheitToCelsius(grades);
-                case "De Fahrenheit a Kelvin" -> Temperatures.FahrenheitToKelvin(grades);
-                case "De Fahrenheit a Rankine" -> Temperatures.FahrenheitToRankine(grades);
+                case "De Celsius a Fahrenheit" -> Temperatures.CelsiusToFahrenheit(grades, "Celsius", "Fahrenheit");
+                case "De Celsius a Kelvin" -> Temperatures.CelsiusToKelvin(grades, "Celsius", "Kelvin");
+                case "De Fahrenheit a Celsius" -> Temperatures.FahrenheitToCelsius(grades, "Fahrenheit", "Celsius");
+                case "De Fahrenheit a Kelvin" -> Temperatures.FahrenheitToKelvin(grades, "Fahrenheit", "Kelvin");
+                case "De Kelvin a Celsius" -> Temperatures.KelvinToCelsius(grades, "Kelvin", "Celsius");
+                case "De Kelvin a Fahrenheit" -> Temperatures.KelvinToFahrenheit(grades, "Kelvin", "Fahrenheit");
             }
 
             int resp = JOptionPane.showConfirmDialog(null, "¿Desea continuar?", "Select an Opion", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
